aef851e853578790e35afa40e56c6cdd46db28db
Will Franceschini <>
2023-04-11 11:09:42 -0700

    basic sever working

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import pg from "pg";
+const { Client } = pg;
+
+const app = express();
+app.use(express.static("public"));  //<-- right after `app` is created and before routes
+
+dotenv.config();  //<-- has to be before 'process.env'
+const port = process.env.PORT || 3000;
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
+app.use(express.json());  //<--has to be before routes
+
+app.get("/", function(req, res) {   
+    res.send("Hello, world!");
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`Server started on port ${port}`);
+    }
+});
