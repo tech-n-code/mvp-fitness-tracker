8cd04f6efb50ddc9abf7874d86f7e967b8bf6f8c
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-08-10 13:13:34 -0700

    moved routes and dq pool to separate files

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { router } from "./src/routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.SERVER_PORT || 3055;
+
+app.use('/api', router);
+
+app.listen(port, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`Server started on port ${port}`);
+    }
+});
